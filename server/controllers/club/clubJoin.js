f40a35103e91073d7c69a158537367bbbb4ed4e3
Jarenchi <jaren719@gmail.com>
2023-11-30 13:51:05 +0800

    fix: fix club database api

--- server/controllers/club/clubJoin.js
+++ server/controllers/club/clubJoin.js
@@ -1,27 +1,34 @@
-const User = require("../../models/userModel");
-const Club = require("../../models/clubModel");
+const userModel = require("../../models/userModel");
+const clubModel = require("../../models/clubModel");
+const memberModel = require("../../models/memberModel");
 
 const clubJoin = async (req, res) => {
   try {
-    const { userId, clubId } = req.body;
-    const user = await User.findById(userId);
-    if (!user) {
+    const { user, clubId } = req.body;
+    const findUser = await userModel.findById(user.id);
+    if (!findUser) {
       return res.status(404).json({ error: "User not found" });
     }
-    const club = await Club.findById(clubId);
+    const club = await clubModel.findById(clubId);
     if (!club) {
       return res.status(404).json({ error: "Club not found" });
     }
-    const isMember = club.members && club.members.some((member) => member.id.equals(userId));
+    const isMember = club.members.includes(user.id);
     if (isMember) {
       return res.status(400).json({ error: "User is already a member of the club" });
     }
-    club.members.push(userId);
-    user.clubs.push({
-      clubId: club._id,
-      clubName: club.name,
+
+    const newMember = new memberModel({
+      userId: user.id,
+      name: user.name,
+      picture: user.picture,
+      clubId: clubId,
     });
-    await Promise.all([user.save(), club.save()]);
+    club.members.push(user.id);
+    await userModel.findByIdAndUpdate(user.id, {
+      $push: { clubs: { id: club._id, name: club.name } },
+    });
+    await Promise.all([club.save(), newMember.save()]);
     res.status(200).json({ message: "User joined the club successfully" });
   } catch (error) {
     console.error(error);
