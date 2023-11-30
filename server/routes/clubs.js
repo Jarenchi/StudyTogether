7658990f5a792e35ef7ad7f91bbf7f103e473e12
Jarenchi <jaren719@gmail.com>
2023-11-30 17:10:32 +0800

    feat: add getMember API and add frontend page

--- server/routes/clubs.js
+++ server/routes/clubs.js
@@ -12,6 +12,7 @@ const { getDocById } = require("../controllers/docs/getDocById");
 const { updateDocTitle } = require("../controllers/docs/updateDocTitle");
 const { updateDocContent } = require("../controllers/docs/updateDocContent");
 const { deleteDoc } = require("../controllers/docs/deleteDoc");
+const { getAllMembers } = require("../controllers/members/getAllMembers");
 
 const router = express.Router();
 router.post("/", verifyAccessToken, checkFormData, uploadImage, clubCreate);
@@ -24,5 +25,7 @@ router.get("/:clubId/docs/:docId", verifyAccessToken, getDocById);
 router.put("/:clubId/docs/:docId/title", verifyAccessToken, updateDocTitle);
 router.put("/:clubId/docs/:docId", verifyAccessToken, updateDocContent);
 router.delete("/:clubId/docs/:docId", verifyAccessToken, deleteDoc);
+
+router.get("/:clubId/members", getAllMembers);
 
 module.exports = router;
