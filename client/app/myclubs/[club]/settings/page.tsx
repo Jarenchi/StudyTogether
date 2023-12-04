802c5cbf43fca8d9140d25d9b2379c80c26de5f4
Jarenchi <jaren719@gmail.com>
2023-12-04 22:41:55 +0800

    feat: add settings page

---
+++ client/app/myclubs/[club]/settings/page.tsx
@@ -0,0 +1,11 @@
+import LeaveClubButton from "@/components/settings/LeaveClubButton";
+
+const Page = () => {
+  return (
+    <div>
+      <LeaveClubButton />
+    </div>
+  );
+};
+
+export default Page;
