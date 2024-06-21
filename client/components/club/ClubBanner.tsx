16619f1f1b47b697f886befe11706686d59814d0
Jarenchi <jaren719@gmail.com>
2024-06-21 11:43:07 +0800

    fix: change layort style

--- client/components/club/ClubBanner.tsx
+++ client/components/club/ClubBanner.tsx
@@ -1,7 +1,7 @@
-import { Clock, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
 import { Club } from "@/types/clubType";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Clock, User } from "lucide-react";
 
 interface ClubBannerProps {
   data: Club;
@@ -11,30 +11,28 @@ const ClubBanner: React.FC<ClubBannerProps> = ({ data }) => {
   const date = new Date(data?.createdAt);
   const formattedDate = date.toLocaleDateString();
   return (
-    <Card className="w-[64rem] p-6">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          <Avatar className="w-36 h-36">
-            <AvatarImage src={data.picture} />
-            <AvatarFallback>{data.name}</AvatarFallback>
+    <Card className="w-5/6 p-6">
+      <div className="flex items-center">
+        <Avatar className="lg:w-24 lg:h-24 w-6 h-6">
+          <AvatarImage src={data.picture} />
+          <AvatarFallback>{data.name}</AvatarFallback>
+        </Avatar>
+        <h1 className="2xl:text-3xl xl:text-2xl lg:text-xl md:text-base text-sm ml-4 break-words">{data.name}</h1>
+      </div>
+      <div>
+        <div className="flex items-center mt-2 md:text-base text-sm">
+          <User />
+          <span className="mr-4 ml-2">Owner :</span>
+          <Avatar>
+            <AvatarImage src={data.owner.picture} />
+            <AvatarFallback>{data.owner.name}</AvatarFallback>
           </Avatar>
-          <h1 className="text-3xl ml-4">{data.name}</h1>
+          <span className="ml-2">{data.owner.name}</span>
         </div>
-        <div>
-          <div className="flex items-center mt-2">
-            <User />
-            <span className="mr-4 ml-2">Owner :</span>
-            <Avatar>
-              <AvatarImage src={data.owner.picture} />
-              <AvatarFallback>{data.owner.name}</AvatarFallback>
-            </Avatar>
-            <span className="ml-2">{data.owner.name}</span>
-          </div>
-          <div className="flex">
-            <Clock />
-            <span className="mr-4 ml-2">CreatedAt :</span>
-            <p>{formattedDate}</p>
-          </div>
+        <div className="flex items-center mt-2 md:text-base text-sm">
+          <Clock />
+          <span className="mr-4 ml-2">CreatedAt :</span>
+          <p>{formattedDate}</p>
         </div>
       </div>
     </Card>
