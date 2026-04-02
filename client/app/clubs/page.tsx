7212f250289658c3d439e87b90f95e9dad5670dc
Jarenchi <jaren719@gmail.com>
2026-04-02 21:26:31 +0800

    fix clublist

--- client/app/clubs/page.tsx
+++ client/app/clubs/page.tsx
@@ -1,19 +1,25 @@
+import { Suspense } from "react";
 import CreateClubButton from "@/components/clubs/CreateClubButton";
 import JoinClubButton from "@/components/clubs/JoinClubButton";
 import ClubList from "@/components/clubs/ClubList";
 import Searchbar from "@/components/clubs/Searchbar";
+import ClubListSkeleton from "@/components/clubs/ClubListSkeleton";
 
 const Page = () => {
   return (
     <div className="mt-3 lg:mx-20 mx-2">
       <div className="mb-3 flex justify-between items-center">
-        <Searchbar />
+        <Suspense>
+          <Searchbar />
+        </Suspense>
         <div className="flex items-center gap-3">
           <CreateClubButton />
           <JoinClubButton />
         </div>
       </div>
-      <ClubList />
+      <Suspense fallback={<ClubListSkeleton />}>
+        <ClubList />
+      </Suspense>
     </div>
   );
 };
